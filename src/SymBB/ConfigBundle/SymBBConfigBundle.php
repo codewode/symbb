1716005f9426e9d0e431671607d9616c69268b15
=Christian Wielath <info@script-brain.de>
2013-07-18 00:21:40 +0200

    setting up configuration and entity manager

---
+++ src/SymBB/ConfigBundle/SymBBConfigBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SymBB\ConfigBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SymBBConfigBundle extends Bundle
+{
+}
---
+++ src/SymBB/UserBundle/SymBBUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SymBB\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SymBBUserBundle extends Bundle
+{
+}
